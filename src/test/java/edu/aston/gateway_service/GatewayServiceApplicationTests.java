2640d29f114960afe11a9f93117d9ce90dfbd316
yafizrug <sanko.veselkin@bk.ru>
2025-12-29 09:47:32 +0300

    first commit

---
+++ src/test/java/edu/aston/gateway_service/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.aston.gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
